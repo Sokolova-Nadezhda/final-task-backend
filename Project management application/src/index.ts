6c31797c129156f9387fb5400eebb204b6aa69cb
unknown <sokolova.nadezhda.31@gmail.com>
2024-02-09 15:58:26 +0200

    init

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://andrewostrovnevtest:nagbWWfxeW4VL80q@cluster0.tb9xzyo.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
